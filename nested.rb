d8d97a88b56cc794a22316c3c1aabeb11263fe83
ppidcard <tvxqlikei@live.cn>
2021-06-29 03:37:09 +1000

    Done

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
@@ -36,7 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,6 +83,13 @@ def adding_matz
       }
     }
 
+    programmer_hash = {
+      yukihiro_matsumoto: {
+        known_for: "Ruby",
+        languages: ["LISP", "C"]
+    }}
+
+    programmer_hash
 end
 
 def changing_alan
@@ -103,6 +111,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -126,4 +136,6 @@ def adding_to_dennis
       }
     }
 
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
